0f46020c1ab7d522fe63b97683c118efdc1ed71f
Lion011095 <lion011095@gmail.con>
2023-10-23 11:36:44 -0400

    Build color mode switch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
-import { HStack, Text, Image } from "@chakra-ui/react";
+import { HStack, Image, useColorModeValue } from "@chakra-ui/react";
 import lionLogo from "../assets/LionLogo.webp";
+import ColorThemeSwitch from "./ColorThemeSwitch";
 
 const Header = () => {
+  const bg = useColorModeValue("orange.400", "blackAlpha.700");
+  const textColor = useColorModeValue("black", "orange.300");
+
   return (
-    <HStack paddingX={"5px"}>
+    <HStack
+      color={textColor}
+      bg={bg}
+      paddingX={"5px"}
+      justifyContent={"space-between"}
+    >
       <Image marginBottom="2px" src={lionLogo} boxSize={10} />
-      <Text>Menu</Text>
-      <Text>Header</Text>
+      <ColorThemeSwitch />
     </HStack>
   );
 };
